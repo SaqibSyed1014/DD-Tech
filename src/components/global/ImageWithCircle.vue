b5152fa02e66f80cee14d3242685d1348f460231
Saqib Syed <sa@techscale.io>
2023-06-26 00:42:14 +0500

    imgs path trial fix x3

--- src/components/global/ImageWithCircle.vue
+++ src/components/global/ImageWithCircle.vue
@@ -3,11 +3,13 @@
     class='image-circle mx-auto lg:mr-0 lg:ml-auto'
     :style='{ background: props.circleColor, justifyContent: props.imagePosition }'
   >
-    <img :src='props.imagePath' :alt='props.alt'>
+    <img :src='getImagePath' :alt='props.alt'>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+
 const props = defineProps({
   imagePath: String,
   alt: String,
@@ -16,6 +18,11 @@ const props = defineProps({
     type: String,
     default: 'center'
   }
+})
+
+const getImagePath = computed(() => {
+  console.log('paths ', import.meta.env.BASE_URL, props.imagePath)
+  return import.meta.env.BASE_URL + props.imagePath
 })
 </script>
 
